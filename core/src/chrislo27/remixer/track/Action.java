4d7e72511738ba93608841f091095b5cb91c1e91
chrislo27 <kinchrislo@gmail.com>
2016-05-26 21:30:34 -0700

    Implements comparable

--- core/src/chrislo27/remixer/track/Action.java
+++ core/src/chrislo27/remixer/track/Action.java
@@ -1,6 +1,6 @@
 package chrislo27.remixer.track;
 
-public abstract class Action {
+public abstract class Action implements Comparable {
 
 	public float beat = 0;
 	public boolean isCompleted = false;
@@ -15,6 +15,18 @@ public abstract class Action {
 
 	public void reset() {
 		isCompleted = false;
+	}
+
+	@Override
+	public int compareTo(Object o) {
+		if (o instanceof Action) {
+			Action other = (Action) o;
+
+			if (other.beat > this.beat) return -1;
+			if (other.beat < this.beat) return 1;
+		}
+
+		return 0;
 	}
 
 }
