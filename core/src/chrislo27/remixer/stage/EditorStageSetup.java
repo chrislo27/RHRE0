dcb1770e4624706ce694fe60a738292530a62b79
chrislo27 <kinchrislo@gmail.com>
2016-05-28 18:22:29 -0700

    Removed status

--- core/src/chrislo27/remixer/stage/EditorStageSetup.java
+++ core/src/chrislo27/remixer/stage/EditorStageSetup.java
@@ -24,7 +24,6 @@ public class EditorStageSetup {
 
 	private Group toolbar;
 	private ImageButton saveButton;
-	private TextLabel status;
 
 	private Group confirmationGroup;
 	private ImageButton confirmationYes;
@@ -111,11 +110,6 @@ public class EditorStageSetup {
 
 			exitGame.getColor().set(0.85f, 0.25f, 0.25f, 1);
 			toolbar.addActor(exitGame).align(Align.topRight).setPixelOffset(8, 8, 32, 32);
-
-			status = new TextLabel(stage, palette, "status.nothing");
-
-			toolbar.addActor(status).setTextAlign(Align.center).align(Align.top | Align.center)
-					.setScreenOffsetSize(1, 0).setPixelOffsetSize(0, 48);
 
 			TextButton interval = new TextButton(stage, palette, "") {
 
