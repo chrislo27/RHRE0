223bcb957bfca1649e6fd8039c4783082d8f717a
chrislo27 <kinchrislo@gmail.com>
2016-06-02 21:29:26 -0700

    Advance version

--- core/src/chrislo27/remixer/Main.java
+++ core/src/chrislo27/remixer/Main.java
@@ -46,7 +46,7 @@ public class Main extends ionium.templates.Main {
 	@Override
 	public void create() {
 		resizeScreenFromSettings();
-		Main.version = "v0.2.1-alpha";
+		Main.version = "v0.3.0-alpha";
 
 		super.create();
 
