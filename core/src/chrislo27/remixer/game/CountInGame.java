253fa6181177e61cb2fc1250e06e981ec8208574
chrislo27 <kinchrislo@gmail.com>
2016-06-09 15:53:40 -0700

    Removed patterns

--- core/src/chrislo27/remixer/game/CountInGame.java
+++ core/src/chrislo27/remixer/game/CountInGame.java
@@ -9,60 +9,8 @@ public class CountInGame extends Game {
 
 	public CountInGame(String name) {
 		super(name, null);
-		
+
 		this.sortingPriority = Short.MAX_VALUE;
-
-		this.patterns.put("and", new Pattern(this) {
-
-			@Override
-			public void addPatternToArray(Array<SoundEffect> array) {
-				array.add(new SoundEffect(0, "countIn_and"));
-			}
-		});
-		this.patterns.put("cowbell", new Pattern(this) {
-
-			@Override
-			public void addPatternToArray(Array<SoundEffect> array) {
-				array.add(new SoundEffect(0, "countIn_cowbell"));
-			}
-		});
-		this.patterns.put("one", new Pattern(this) {
-
-			@Override
-			public void addPatternToArray(Array<SoundEffect> array) {
-				array.add(new SoundEffect(0, "countIn_one"));
-			}
-		});
-		this.patterns.put("two", new Pattern(this) {
-
-			@Override
-			public void addPatternToArray(Array<SoundEffect> array) {
-				array.add(new SoundEffect(0, "countIn_two"));
-			}
-		});
-		this.patterns.put("three", new Pattern(this) {
-
-			@Override
-			public void addPatternToArray(Array<SoundEffect> array) {
-				array.add(new SoundEffect(0, "countIn_three"));
-			}
-		});
-		this.patterns.put("four", new Pattern(this) {
-
-			@Override
-			public void addPatternToArray(Array<SoundEffect> array) {
-				array.add(new SoundEffect(0, "countIn_four"));
-			}
-		});
-	}
-
-	@Override
-	public int compareTo(Object o) {
-		if (o instanceof Game) {
-			return -1;
-		}
-
-		return super.compareTo(o);
 	}
 
 }
