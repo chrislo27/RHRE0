f5b5144bcd269ad806be45481193170078f538cf
chrislo27 <kinchrislo@gmail.com>
2016-06-18 19:12:48 -0700

    Fixed fullscreen

--- core/src/chrislo27/remixer/init/IoniumEngineTweaks.java
+++ core/src/chrislo27/remixer/init/IoniumEngineTweaks.java
@@ -8,6 +8,9 @@ public class IoniumEngineTweaks {
 	public static final void tweak() {
 		GlobalVariables.ticks = 20;
 		GlobalVariables.versionUrl = "https://raw.githubusercontent.com/chrislo27/VersionPlace/master/RHRE-version.txt";
+		GlobalVariables.defaultFullscreen = false;
+		GlobalVariables.defaultWidth = 1280;
+		GlobalVariables.defaultHeight = 720;
 
 		DebugSetting.showFPS = false;
 	}
