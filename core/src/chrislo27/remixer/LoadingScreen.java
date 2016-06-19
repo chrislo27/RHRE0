127f1a05c3c23a6701e584feb7a7a3be165c6b00
chrislo27 <kinchrislo@gmail.com>
2016-06-19 08:34:04 -0700

    Added splash screen

--- core/src/chrislo27/remixer/LoadingScreen.java
+++ core/src/chrislo27/remixer/LoadingScreen.java
@@ -1,5 +1,6 @@
 package chrislo27.remixer;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 
 import ionium.screen.AssetLoadingScreen;
@@ -9,8 +10,55 @@ import ionium.util.packer.TileTexturePacker;
 
 public class LoadingScreen extends AssetLoadingScreen {
 
+	private static final float SPLASH_FADE = 0.5f;
+
+	private float splashOpacity = 1;
+	private float splashDecay = 2;
+	private Texture splash;
+
 	public LoadingScreen(Main m) {
 		super(m);
+
+		splash = new Texture("images/splash.png");
+	}
+
+	@Override
+	public void render(float delta) {
+		super.render(delta);
+
+		main.batch.begin();
+
+		main.batch.setColor(0, 0, 0, splashOpacity);
+		Main.fillRect(main.batch, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		main.batch.setColor(1, 1, 1, splashOpacity);
+		float width = (splash.getWidth() * 1f / splash.getHeight()) * Gdx.graphics.getHeight();
+		main.batch.draw(splash, Gdx.graphics.getWidth() * 0.5f - width * 0.5f, 0, width,
+				Gdx.graphics.getHeight());
+
+		main.batch.setColor(1, 1, 1, 1);
+
+		main.batch.end();
+	}
+
+	@Override
+	public void renderUpdate() {
+		super.renderUpdate();
+
+		if (shouldFinishLoading() && splashDecay > 0) splashDecay = 0;
+
+		if (splashDecay <= 0) {
+			splashDecay = 0;
+
+			splashOpacity = Math.max(splashOpacity - Gdx.graphics.getDeltaTime() / SPLASH_FADE, 0);
+		} else {
+			splashDecay -= Gdx.graphics.getDeltaTime();
+		}
+	}
+
+	@Override
+	public boolean canFinishLoading() {
+		return splashOpacity <= 0;
 	}
 
 	@Override
@@ -22,30 +70,37 @@ public class LoadingScreen extends AssetLoadingScreen {
 		ttp.mustUsePowerOfTwo = true;
 		ttp.maxTextureSize = 512;
 
-//		Array<Texture> temp = new Array<>();
-//
-//		for (Game g : GameList.instance().games.getAllValues()) {
-//			ttp.addTexture(g.name, new Texture("images/games/" + g.name + ".png"));
-//		}
-//
-//		ttp.pack();
-//
-//		GameList.instance().atlas = ttp.getPackedTexture();
-//		Array<AtlasRegion> all = GameList.instance().atlas.getRegions();
-//		GameList.instance().allRegions = new ObjectMap<>();
-//
-//		for (AtlasRegion region : all) {
-//			GameList.instance().allRegions.put(region.name, region);
-//		}
-//
-//		for (Texture t : temp) {
-//			t.dispose();
-//		}
+		//		Array<Texture> temp = new Array<>();
+		//
+		//		for (Game g : GameList.instance().games.getAllValues()) {
+		//			ttp.addTexture(g.name, new Texture("images/games/" + g.name + ".png"));
+		//		}
+		//
+		//		ttp.pack();
+		//
+		//		GameList.instance().atlas = ttp.getPackedTexture();
+		//		Array<AtlasRegion> all = GameList.instance().atlas.getRegions();
+		//		GameList.instance().allRegions = new ObjectMap<>();
+		//
+		//		for (AtlasRegion region : all) {
+		//			GameList.instance().allRegions.put(region.name, region);
+		//		}
+		//
+		//		for (Texture t : temp) {
+		//			t.dispose();
+		//		}
 
 		if (DebugSetting.debug) {
 			Main.filltex = new Texture("images/games/moaiDooWop2.png");
 			Main.filltexRegion.setTexture(Main.filltex);
 		}
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+
+		splash.dispose();
 	}
 
 }
